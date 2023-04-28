c05bf03dafe382194f9fcbcac02a1d34af73e9d8
terry <taerimyoon@korea.ac.kr>
2023-04-28 17:04:24 +0900

    add ELASTICA

---
+++ python/EXPERIMENT_ELASTICA/ELASTICA_TENDON.py
@@ -0,0 +1,351 @@
+import sys
+
+from pathlib import Path
+
+try: 
+    get_ipython().__class__.__name__
+    BASEDIR = Path().absolute()
+except: 
+    BASEDIR = Path(__file__).parent
+
+
+gettrace = getattr(sys, 'gettrace', None)
+if gettrace() is None:
+    print('Run Mode')
+    DEBUG_MODE = False
+    
+elif gettrace():
+    print('Debugging Mode... Skip Logging...')
+    DEBUG_MODE = True
+    
+else:
+    print("Let's do something interesting")
+    sys.exit(0)
+
+runname = "[ELASTICA]BATCH64"
+
+folder = BASEDIR / 'ELASTICA_RESULT' / runname
+sys.path.append(str(BASEDIR/".."))
+
+import time
+import numpy as np
+import scipy.optimize
+import pickle
+from tqdm import tqdm
+
+from py_diff_pd.common.common import ndarray, create_folder, rpy_to_rotation, rpy_to_rotation_gradient
+from py_diff_pd.common.common import print_info, print_ok, print_error
+from py_diff_pd.common.grad_check import check_gradients
+from py_diff_pd.core.py_diff_pd_core import StdRealVector
+from EXPERIMENT_FINGER.env.FINGER_RountingEnv import CRoutingTendonEnv3d 
+from EXPERIMENT_FINGER.dataloader import get_dataset, Sampler
+from functools import partial
+import wandb
+import torch
+import random
+
+
+SEED = 1
+torch.manual_seed(SEED)
+torch.cuda.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+random.seed(SEED)
+
+# lr = 2
+
+PROPORTION_MATRIX = np.array(
+    [
+        [1,0.1,0.4,0.4, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+        [0.1,1,0.4,0.4, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+        [0.4,0.4,1,0.1, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+        [0.4,0.4,0.1,1, 0,0,0,0, 0,0,0,0, 0,0,0,0],
+        
+        [0,0,0,0,  1,0.1,0.4,0.4, 0,0,0,0, 0,0,0,0],
+        [0,0,0,0,  0.1,1,0.4,0.4,  0,0,0,0, 0,0,0,0],
+        [0,0,0,0,  0.4,0.4,1,0.1, 0,0,0,0, 0,0,0,0],
+        [0,0,0,0,  0.4,0.4,0.1,1, 0,0,0,0, 0,0,0,0],
+        
+        [0,0,0,0, 0,0,0,0, 1,0.1,0.4,0.4, 0,0,0,0],
+        [0,0,0,0, 0,0,0,0, 0.1,1,0.4,0.4, 0,0,0,0],
+        [0,0,0,0, 0,0,0,0, 0.4,0.4,1,0.1, 0,0,0,0],
+        [0,0,0,0, 0,0,0,0, 0.4,0.4,0.1,1, 0,0,0,0],
+        
+        [0,0,0,0, 0,0,0,0, 0,0,0,0, 1,0.1,0.4,0.4],
+        [0,0,0,0, 0,0,0,0, 0,0,0,0, 0.1,1,0.4,0.4],
+        [0,0,0,0, 0,0,0,0, 0,0,0,0, 0.4,0.4,1,0.1],
+        [0,0,0,0, 0,0,0,0, 0,0,0,0, 0.4,0.4,0.1,1],
+        ])
+PROPORTION_MATRIX = PROPORTION_MATRIX[:4,:4]
+
+
+if __name__ == '__main__':
+    if not DEBUG_MODE:
+        wandb.init(project = 'DiffPD with ELASTICA')
+        wandb.run.name = str.replace(runname,"_"," ")
+    
+    
+    youngs_modulus = 5e5
+    poissons_ratio = 0.45
+    refinement = 4
+    muscle_cnt = 8
+    env = CRoutingTendonEnv3d(SEED, folder, {
+        'muscle_cnt': muscle_cnt,
+        'refinement': refinement,
+        'youngs_modulus': youngs_modulus,
+        'poissons_ratio': poissons_ratio })
+    deformable = env.deformable()
+    mesh = env.mesh
+    
+    # Camera options
+    env._camera_pos = (0.3, -1, .25)
+    env._scale = 2
+    
+    # Optimization parameters.
+    import multiprocessing
+    cpu_cnt = multiprocessing.cpu_count()
+    thread_ct = 96
+    print_info('Detected {:d} CPUs. Using {} of them in this run'.format(cpu_cnt, thread_ct)) 
+    pd_opt = { 'max_pd_iter': 500, 'max_ls_iter': 10, 'abs_tol': 1e-9, 'rel_tol': 1e-4, 'verbose': 0, 'thread_ct': thread_ct,
+        'use_bfgs': 1, 'bfgs_history_size': 10 }
+    methods = ('pd_eigen',)
+    opts = (pd_opt,)
+
+    dt = 1e-2
+    frame_num = 15
+
+    # Initial state.
+    dofs = deformable.dofs()
+    act_dofs = deformable.act_dofs()
+    q0 = env.default_init_position()
+    v0 = np.zeros(dofs)
+    f0 = [np.zeros(dofs) for _ in range(frame_num)]
+    act_maps = env.act_maps()
+    u_dofs = len(act_maps)
+    node_nums = env._node_nums
+    # assert u_dofs * (refinement ** 3) * muscle_ext == act_dofs
+
+    def variable_to_act(x):
+        act = np.ones(act_dofs)
+        for i, a in enumerate(act_maps):
+            act[a] = 1 - x[i]
+        return act
+    
+    # Get Marker
+    segNumber = 2
+    
+    zcorrdinate = (np.linspace(0,0.12,segNumber)    /env._dx).astype(int)
+    xcoordinate = (np.ones_like(zcorrdinate) * 0.01 /env._dx).astype(int)
+    ycoordinate = (np.ones_like(zcorrdinate) * 0.01 /env._dx).astype(int)
+    
+    py_verticeIdxs = []
+    for m in range(segNumber):
+        if m==0:
+            continue
+        idx = node_nums[2]*node_nums[1]*xcoordinate[m] + node_nums[2]*ycoordinate[m] + zcorrdinate[m]
+        idx = int(idx)
+        py_verticeIdxs.append(idx)
+    py_verticeIdxs = np.array(py_verticeIdxs)
+    
+    env.set_marker(py_verticeIdxs,verbose=True)
+    
+    
+    # Get dataset
+    trainDataset,valDataset,testDataset,extTestDataset = get_dataset(f"python/EXPERIMENT_ELASTICA/ELASTICA.json")
+    
+    # Set method
+    method,opt = methods[0], opts[0]
+    
+    # p_offset
+    idx = torch.argmin(torch.norm(trainDataset.motor_control - torch.tensor([0,0]), dim=1))
+    init_pose = trainDataset.position[idx].flatten()
+    p_offset = np.array([init_pose[0], init_pose[1], 0])
+    
+    def simulate_batch(x, dataset, batchsize=16, vis_folderprefix=None, requires_grad=False, batchidxs = None, prefix = None):
+        if prefix is None:
+            prefix = vis_folderprefix
+            
+        
+        E = np.exp(x[0])
+        nu = np.exp(x[1])
+        
+        if vis_folderprefix==None: savefolder=folder
+        else: savefolder = folder / vis_folderprefix
+        env = CRoutingTendonEnv3d(SEED, savefolder, {
+        'muscle_cnt': muscle_cnt,
+        'refinement': refinement,
+        'youngs_modulus': E,
+        'poissons_ratio': nu})
+        env.set_marker(py_verticeIdxs, verbose=False)
+        
+        # Camera options
+        env._camera_pos = (0.3, -1, .25)
+        env._scale = 2
+        
+        losses = []
+        grads = []
+        
+        if batchidxs is None: 
+            batchidxs = np.random.choice(np.arange(len(dataset)), size=batchsize, replace=False)
+        else:
+            batchsize = len(batchidxs)
+            print("Batchidxs Explicitly Defined")
+        
+        FAILED_NUMBER = 0
+        for batchidx,idx in enumerate(batchidxs):
+            print("Optimizing... batchidx:{}/{}".format(batchidx+1,batchsize))
+            
+            datapoint = dataset.__getitem__(idx)
+            act_raw = datapoint['motor_control']
+            
+            temp = np.zeros(4)
+            if act_raw[0]>0:
+                temp[0] = act_raw[0]
+            else:
+                temp[1] = -act_raw[0]
+                
+            if act_raw[1]>0:
+                temp[2] = act_raw[1]
+            else:
+                temp[3] = -act_raw[1]
+            
+            # act_raw = temp/16_000
+            act_raw = temp/50
+            
+            act_raw = PROPORTION_MATRIX @ act_raw
+            
+            act_raw = act_raw[[2,0,3,1]]
+            
+            markers = datapoint['position'].flatten() - p_offset + torch.tensor([10,10,.0])/1000
+            print('actuation: ', act_raw)
+            print('position:', markers)
+
+            act = variable_to_act(act_raw)
+            env._construct_loss_and_grad(markers)
+            
+            # set output folder path
+            if not vis_folderprefix==None: vis_folder = vis_folderprefix + str(batchidx+1)
+            else: vis_folder = None
+            
+            import json
+            with open(f"{folder}/temp/{vis_folder}_act", 'w') as outfile:
+                json.dump(dict(act=act_raw.tolist()), outfile)
+            
+            act_frames = []
+            for _ in range(frame_num):
+                act_frames.append(act)
+            
+            if requires_grad:
+                try:
+                    loss, _, info = env.simulate(dt, frame_num, method, opt, q0, v0, act_frames, f0, require_grad=requires_grad, vis_folder=vis_folder)
+                except RuntimeError:
+                    print("[WARNING!!] SIMULATION HAS FAILED TO CONVERGE!!")
+                    print("Skipping Current MOTOR COMMAND!!")
+                    FAILED_NUMBER = FAILED_NUMBER + 1
+                    continue
+                grad = info['material_parameter_gradients']
+                print('loss: {:8.3f}, |grad|: {:8.3f}, gradE{:12.3e}, gradNu{:12.3e}, E: {:8.3e}, nu: {:4.3f}, forward time: {:6.3f}s, backward time: {:6.3f}s'.format(
+                    loss, np.linalg.norm(grad), grad[0], grad[1], E, nu, info['forward_time'], info['backward_time']))
+            else:
+                try:
+                    loss, info = env.simulate(dt, frame_num, method, opt, q0, v0, act_frames, f0, require_grad=requires_grad, vis_folder=vis_folder)
+                except RuntimeError:
+                    print("[WARNING!!] SIMULATION HAS FAILED TO CONVERGE!!")
+                    print("Skipping Current MOTOR COMMAND!!")
+                    FAILED_NUMBER = FAILED_NUMBER + 1
+                    continue
+                grad = 0
+                print('loss: {:8.3f}, forward time: {:6.3f}s,'.format(loss, info['forward_time']))
+
+            grad = grad * np.exp(x)
+            if grad[0]<0 and DEBUG_MODE:
+                print("Found!!!")
+                pass
+            losses.append(loss)
+            grads.append(grad)
+        
+        losses,grads = np.array(losses), np.array(grads)
+        avgLoss, avgGrads = losses.mean(0), grads.mean(0)
+        
+        # avgGrads = avgGrads * lr
+        FAILED_RATIO = FAILED_NUMBER / len(batchidxs)
+        if not DEBUG_MODE:
+            if requires_grad:
+                wandb.log(
+                    {prefix+'_L2Loss':avgLoss, prefix+'_L1_Loss':(2*avgLoss)**(1/2), \
+                        prefix+'_logEGrads':float(avgGrads[0]),prefix+'_NuGrads':float(avgGrads[1]), \
+                            prefix+'_E':E, prefix+'_nu':nu, prefix+'_FAILED_RATIO':FAILED_RATIO})
+            else:
+                wandb.log({prefix+'_L1_Loss':(2*avgLoss)**(1/2), prefix+'_FAILED_RATIO':FAILED_RATIO})
+        
+        return avgLoss, avgGrads
+    
+    # ## Initial guess for material parameter.
+    x_lb = ndarray([np.log(5e5), np.log(0.2)])
+    x_ub = ndarray([np.log(1e7), np.log(0.45)])
+    x_init = np.random.uniform(x_lb, x_ub)
+    print_info('Simulating initial solution. Please check out the {}/init folder'.format(folder))
+    
+    ##### Simulate INIT  #####
+    batch_idxs_saved = []
+    
+    idx = torch.argmin(torch.norm(trainDataset.motor_control - torch.tensor([1000,0]), dim=1))
+    batch_idxs_saved.append(idx)
+
+    idx = torch.argmin(torch.norm(trainDataset.motor_control - torch.tensor([-1000,0]), dim=1))
+    batch_idxs_saved.append(idx)
+
+    idx = torch.argmin(torch.norm(trainDataset.motor_control - torch.tensor([0,1000]), dim=1))
+    batch_idxs_saved.append(idx)
+
+    idx = torch.argmin(torch.norm(trainDataset.motor_control - torch.tensor([0,-1000]), dim=1))
+    batch_idxs_saved.append(idx)
+    batch_idxs_saved = np.array(batch_idxs_saved)
+    
+    # loss, info = simulate_batch(x=x_init,dataset = trainDataset, vis_folderprefix='init', requires_grad=False, batchidxs = batchidxs)
+    
+    # if not DEBUG_MODE:
+    loss, info = simulate_batch(x=x_init,dataset = trainDataset, vis_folderprefix='init', requires_grad=True, batchidxs = batch_idxs_saved, prefix = "Init")
+    print("Inital L1 Loss: ", (2*loss)**(1/2))
+    print_info('Initial guess is ready.')
+    # sys.exit(0)
+    
+    
+    ##### Train #####
+    ## Optimization for material parameter
+    Train = True
+     
+    t0 = time.time()
+    if Train:
+        vis_folderprefix = 'train' if DEBUG_MODE else None
+        bounds = scipy.optimize.Bounds(x_lb, x_ub)
+        batchsize = 64
+        simulate_trainbatch = partial(simulate_batch,dataset = trainDataset, batchsize = batchsize, vis_folderprefix = vis_folderprefix, requires_grad=True, prefix = "Train")
+        # simulate_trainbatch = partial(simulate_batch,dataset = trainDataset, batchsize = len(trainDataset), vis_folderprefix = vis_folderprefix, requires_grad=True, prefix="Train")
+        result = scipy.optimize.minimize(simulate_trainbatch, np.copy(x_init),
+            method='L-BFGS-B', jac=True, bounds=bounds, options={ 'ftol': 1e-2, 'maxiter': 100 })
+        
+        print(result.success)
+        x_final = result.x
+    else:
+        x_final = x_init
+    t1 = time.time()
+    
+    E  = np.exp(x_final[0])
+    nu = np.exp(x_final[1])
+    if not DEBUG_MODE: wandb.log({'E':E, 'nu':nu})
+    print_info('Optimizing with {} finished in {:6.3f} seconds'.format(method, t1 - t0))
+
+    ##### Test #####
+    loss, info = simulate_batch(x=x_init, dataset = trainDataset, vis_folderprefix='final', requires_grad=False, batchidxs = batch_idxs_saved, prefix = "Final")
+    
+    loss, info = simulate_batch(x=x_final, dataset = valDataset,batchsize=len(valDataset), requires_grad=False, prefix = "Val")
+    
+    loss, info = simulate_batch(x=x_final, dataset = testDataset,batchsize=len(testDataset), requires_grad=False, prefix = "Test")
+    
+    loss, info = simulate_batch(x=x_final, dataset = extTestDataset,batchsize=len(extTestDataset), requires_grad=False, prefix = "extTest")
+    
+    wandb.finish()
+
+
---
+++ python/EXPERIMENT_ELASTICA/env/FINGER_RountingEnv.py
@@ -0,0 +1,267 @@
+import time
+from pathlib import Path
+
+import numpy as np
+
+
+from py_diff_pd.core.py_diff_pd_core import StdRealVector, StdIntVector
+
+from py_diff_pd.env.env_base import EnvBase
+from py_diff_pd.common.common import create_folder, ndarray, copy_std_int_vector
+from py_diff_pd.common.hex_mesh import generate_hex_mesh
+from py_diff_pd.common.display import export_gif
+from py_diff_pd.common.project_path import root_path
+from py_diff_pd.common.renderer import PbrtRenderer
+from py_diff_pd.core.py_diff_pd_core import HexMesh3d, HexDeformable, StdRealVector
+from functools import partial
+
+def get_centerpoint_from_cell(mesh, idx):
+    vertices_idx = mesh.py_element(idx)
+    
+    vertices_points = []
+    
+    for vertex_idx in vertices_idx:
+        vertices_points.append(np.array(mesh.py_vertex(vertex_idx)))
+    
+    # return np.stack(vertices_points)
+    
+    return np.mean(np.stack(vertices_points),axis=0)
+    
+
+class CRoutingTendonEnv3d(EnvBase):
+    def __init__(self, seed, folder, options):
+        EnvBase.__init__(self, folder)
+
+        create_folder(folder, exist_ok=True)
+
+        muscle_cnt = options['muscle_cnt']
+        
+        refinement = options['refinement']
+        youngs_modulus = options['youngs_modulus']
+        poissons_ratio = options['poissons_ratio']
+        actuator_parameters = options['actuator_parameters'] if 'actuator_parameters' in options else ndarray([5.,])
+
+        # Mesh parameters.
+        la = youngs_modulus * poissons_ratio / ((1 + poissons_ratio) * (1 - 2 * poissons_ratio))
+        mu = youngs_modulus / (2 * (1 + poissons_ratio))
+        density = 1e3
+        # Mesh size: 2 x 2 x (muscle_cnt x muscle_ext).
+        cell_nums = (refinement, refinement, 6 * refinement)
+        origin = ndarray([0, 0, 0])
+        node_nums = tuple(n + 1 for n in cell_nums)
+        dx = 0.02 / refinement
+        bin_file_name = folder / 'mesh.bin'
+        voxels = np.ones(cell_nums)
+        generate_hex_mesh(voxels, dx, origin, bin_file_name)
+        mesh = HexMesh3d()
+        mesh.Initialize(str(bin_file_name))
+
+        deformable = HexDeformable()
+        deformable.Initialize(str(bin_file_name), density, 'neohookean', youngs_modulus, poissons_ratio)
+        # Boundary conditions.
+        for i in range(node_nums[0]):
+            for j in range(node_nums[1]):
+                node_idx = i * node_nums[1] * node_nums[2] + j * node_nums[2]
+                vx, vy, vz = mesh.py_vertex(node_idx)
+                deformable.SetDirichletBoundaryCondition(3 * node_idx, vx)
+                deformable.SetDirichletBoundaryCondition(3 * node_idx + 1, vy)
+                deformable.SetDirichletBoundaryCondition(3 * node_idx + 2, vz)
+        # Elasticity.
+        deformable.AddPdEnergy('corotated', [2 * mu,], [])
+        deformable.AddPdEnergy('volume', [la,], [])
+        # Actuation.
+        element_num = mesh.NumOfElements()
+        act_indices = list(range(element_num))
+        actuator_stiffness = self._actuator_parameter_to_stiffness(actuator_parameters)
+        deformable.AddActuation(actuator_stiffness[0], [0.0, 0.0, 1.0], act_indices)
+        act_maps = []
+        
+        # Tendoncoordinate =(
+        #     [(1,3),(1,7)],
+        #     [(3,9),(7,9)],
+        #     [(9,7),(9,3)],
+        #     [(7,1),(3,1)]
+        #     )
+        print("Vertices: \n", np.array(mesh.py_vertices()).reshape(-1,3))
+        print("Cell: \n", )
+        for i in range(cell_nums[0]):
+            idx = cell_nums[2]*cell_nums[1]*i
+            cell_position = get_centerpoint_from_cell(mesh,idx)
+            print(cell_position)
+            
+        # Tendoncoordinate =(
+        #     [(0,0),(1,0),(2,0),(3,0)],
+        #     )
+        
+        if refinement == 4:
+            Tendoncoordinate =(
+                [(0,1),(0,2)],
+                [(1,3),(2,3)],
+                [(3,2),(3,1)],
+                [(1,0),(3,0)],
+                )
+        else: raise Exception("Undefined Tendoncoordinate for specific 'options['refinement']'")
+        
+        for actutation in Tendoncoordinate:
+            act_map = []
+            for tendon in actutation:
+                for z in range(cell_nums[2]):
+                    idx = cell_nums[2]*cell_nums[1]*tendon[0] + cell_nums[2]*tendon[1] + z
+                    act_map.append(idx)
+                    
+                    if z == 0:
+                        cell_position = get_centerpoint_from_cell(mesh,idx)
+                        print("Tendon located at: \n", cell_position)
+            act_maps.append(act_map)
+            
+        self.__act_maps = act_maps
+
+        dofs = deformable.dofs()
+        act_dofs = deformable.act_dofs()
+        q0 = ndarray(mesh.py_vertices())
+        v0 = np.zeros(dofs)
+        f_ext = np.zeros(dofs)
+
+        # Data members.
+        self.mesh = mesh
+        self._deformable = deformable
+        self._q0 = q0
+        self._v0 = v0
+        self._youngs_modulus = youngs_modulus
+        self._poissons_ratio = poissons_ratio
+        self._actuator_parameters = actuator_parameters
+        self._f_ext = f_ext
+        self._stepwise_loss = False
+        self._dx = dx
+        self._node_nums = node_nums
+        self._cell_nums = cell_nums
+        self.__spp = options['spp'] if 'spp' in options else 4
+
+        self._set_display_options('top_view')
+    # def material_stiffness_differential(self, youngs_modulus, poissons_ratio):
+    #     # This (0, 2) shape is due to the usage of Neohookean materials.
+    #     return np.zeros((0, 2))
+    
+    def material_stiffness_differential(self, youngs_modulus, poissons_ratio):
+        jac = self._material_jacobian(youngs_modulus, poissons_ratio)
+        jac_total = np.zeros((2, 2))
+        jac_total[0] = 2 * jac[1]
+        jac_total[1] = jac[0]
+        return jac_total
+
+    def is_dirichlet_dof(self, dof):
+        k = dof % self.__node_nums[2]
+        return k == 0
+
+    def act_maps(self):
+        return self.__act_maps
+
+    def set_marker(self, py_verticeIdxs, verbose):
+        if verbose:
+            # Print marker position
+            for elementidx in py_verticeIdxs:
+                elementidx = int(elementidx)
+                markerposition = self.mesh.py_vertex(elementidx)
+                print("Marker set to [x:{},y:{},z:{}]".format(markerposition[0],markerposition[1],markerposition[2]))
+        
+        # Idxs of vertice -> (x,y,z) combined
+        self.py_verticeIdxs = py_verticeIdxs
+        Markernumber = py_verticeIdxs.shape[0]
+        # Idxs of vertice -> (x),(y),(z)
+        self.py_verticesIdxs = np.zeros(Markernumber*3,dtype=np.int)
+        self.markerVertices = []
+        
+        for idx,elementidx in enumerate(self.py_verticeIdxs):
+            
+            self.py_verticesIdxs[idx*3] = elementidx*3
+            self.py_verticesIdxs[idx*3+1] = elementidx*3 +1
+            self.py_verticesIdxs[idx*3+2] = elementidx*3 + 2
+            
+            # markerpos = mesh.py_vertex(int(elementidx))
+            # self.markerVertices.append(list(markerpos))
+        
+        NumOfq0 = self._q0.shape[0]
+        self.onehot_py_verticesIdxs = np.eye(NumOfq0)[self.py_verticesIdxs]
+        self.onehot_py_verticesIdxs = np.sum(self.onehot_py_verticesIdxs,axis=0,dtype=np.bool)
+    
+    def _set_display_options(self,options='top_view'):
+        possible_options = dict(
+            front_view = {
+                'light_map': 'uffizi-large.exr',
+                'sample': self.__spp,
+                'max_depth': 2,
+                'camera_pos': (0.4, -1., .25),
+                'camera_lookat': (0, .15, .15),
+                },
+            top_view = {
+                'light_map': 'uffizi-large.exr',
+                'sample': self.__spp,
+                'max_depth': 2,
+                'camera_pos': (0.1, 0.1, 1.5),
+                'camera_lookat': (0, .0, -3.1415/2),
+            })
+
+        if type(options) is str:
+            self.options = possible_options[options]
+        elif type(options) is dict:
+            self.options = options
+        else:
+            print("Invalid diplay options!!!")
+            print("Setting to default options!!")
+            self._set_display_options()
+        
+            
+    
+    def _display_mesh(self, mesh_file, file_name):
+        # Render.
+        options = self.options
+        options['file_name'] = file_name
+            
+        renderer = PbrtRenderer(options)
+
+        mesh = HexMesh3d()
+        mesh.Initialize(mesh_file)
+        renderer.add_hex_mesh(mesh, render_voxel_edge=True, color=(.3, .7, .5), transforms=[
+            ('s', self._scale),
+        ])
+        renderer.add_tri_mesh(Path(root_path) / 'asset/mesh/curved_ground.obj',
+            texture_img='chkbd_24_0.7', transforms=[('s', 2)])
+
+        # display marker
+        q = ndarray(mesh.py_vertices())
+        markers = q[self.py_verticesIdxs].reshape(-1,3)
+        for marker in markers:
+            renderer.add_shape_mesh({ 'name': 'sphere', 'center': marker, 'radius': 0.005 },
+                transforms=[('s', self._scale)], color=(0.9, 0.1, 0.1))
+        
+        markers = self.markers.reshape(-1,3)
+        for marker in markers:
+            renderer.add_shape_mesh({ 'name': 'sphere', 'center': marker, 'radius': 0.005 },
+                transforms=[('s', self._scale)], color=(0.1, 0.1, 0.9))
+        
+        renderer.render()
+
+    def _general_loss_and_grad(self, q, v, markers):
+        # Construct target
+        q_target = np.zeros_like(self.mesh.py_vertices())
+        
+        assert markers.shape == self.py_verticesIdxs.shape
+        for id,idx in enumerate(self.py_verticesIdxs):
+            q_target[idx] = markers[id]
+        
+        # Construct estimation
+        q = np.where(self.onehot_py_verticesIdxs,q,0)
+        
+        # calculate grad & loss
+        grad = q - q_target
+        grad = grad * 1000
+        
+        loss = 0.5 * grad.dot(grad)
+        loss = loss
+        return loss, grad, np.zeros(q.size)
+    
+    def _construct_loss_and_grad(self, markers):
+        markers = ndarray(markers).reshape(-1)
+        self.markers = markers
+        self._loss_and_grad = partial(self._general_loss_and_grad, markers = markers)
+    
